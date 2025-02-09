ff166438cd8bc31e405dc43d842a46ff245a5337
Rutabana <rutabana12loic@gmail.com>
2025-02-09 18:14:59 -0500

    Slight Tweak to README & updated template_script for new workflow

--- src/EC2/template_script.sh
+++ src/EC2/template_script.sh
@@ -2,52 +2,17 @@
 # Update packages and install python3 if not already installed
 yum update -y
 yum install -y python3
+yum install -y git
 
 # Change to a working directory (optional)
 cd /home/ec2-user
+# Check if the directory Project-Nairobi exists
+if [ -d "Project-Nairobi" ]; then
+  cd /Project-Nairobi
+  git pull
+else
+  git clone https://github.com/Rutabana/Project-Nairobi.git
+  cd /Project-Nairobi
+fi
 
-# Create the simulation script file
-cat > phone.py << 'EOF'
-#!/usr/bin/env python3
-import time
-import json
-import boto3
-from botocore.exceptions import ClientError
-
-kinesis_client = boto3.client('kinesis', region_name='us-east-2')
-stream_name ='nairobi-stream'
-device_id = 'phone-123'
-
-def simulate():
-  while True:
-    # Construct the JSON payload
-    payload = {
-      "deviceId": device_id,
-      "timestamp": int(time.time()),
-      "status": "ping"
-    }
-
-    # Convert to bytes
-    data_bytes = json.dumps(payload).encode('utf-8')
-
-    try:
-      response = kinesis_client.put_record(
-        StreamName=stream_name,
-        Data=data_bytes,
-        ParitionKey=device_id,
-      )
-      print(f"Send record {response['SequenceNumber']}")
-    except ClientError as e:
-      print(f"Failed to send record: {e}")
-      # TODO: Implement a backoff strategy here
-    
-    time.sleep(3) #TODO: change this to 10 when I know it's working
-
-if __name__ == '__main__':
-  simulate()EOF
-
-# Make the script executable
-chmod +x phone.py
-
-# Run the simulation script in the background
-nohup python3 phone.py > phone.log 2>&1 &
+python3 "/src/EC2/IoT Devices/main.py"
